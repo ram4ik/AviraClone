2f4f6b82b537e80b5a262392b4a19a947ad7501d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-28 10:46:37 +0200

    Initial Commit

---
+++ AviraClone/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AviraClone
+//
+//  Created by Ramill Ibragimov on 28.03.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
